7fd320f0c2bf18e754d63eb10cbf1ba9129cd13a
PothieuG <gillespothieu@ssw.fr>
2025-11-04 16:07:52 +0100

    Implementing Stdio compatible with AI LLM (Claude Desktop, ChatGPT...)

--- src/MCPServer/Program.cs
+++ src/MCPServer/Program.cs
@@ -9,5 +9,13 @@ var ollama = new OllamaService("http://localhost:11434", "gemma3:4b");
 var tools = new ToolsService(ollama);
 var jsonRpc = new JsonRpcService(tools);
 
-var transport = new HttpTransport(jsonRpc);
-await transport.Run();
+if (commandLineArgs.Contains("--stdio"))
+{
+    var transport = new StdioTransport(jsonRpc);
+    await transport.Run();
+}
+else
+{
+    var transport = new HttpTransport(jsonRpc);
+    await transport.Run();
+}
