bb819da7210ae902de2ebeca4873681b7d2aefc8
tr00d <guillaume.faas@live.fr>
2025-11-13 13:56:16 +0100

    fix: use Vonage integrated DI extensions, rename keys in appsettings

--- src/Application/Common/Interface/IVonageService.cs
+++ src/Application/Common/Interface/IVonageService.cs
@@ -1,11 +1,9 @@
-using SSW_x_Vonage_Clean_Architecture.Application.UseCases.Calls.Commands.HandleTranscription;
-
 namespace Vonage_SSW_Workshop.Application.Common.Interfaces;
 
 public interface IVonageService
 {
     Task<string> InitiateCallAsync(string phoneNumber, CancellationToken cancellationToken = default);
-    Task<ErrorOr<TranscriptionResult>> DownloadTranscriptionAsync(string transcriptionUrl, CancellationToken cancellationToken = default);
+    Task<ErrorOr<string>> DownloadTranscriptionAsync(string transcriptionUrl, CancellationToken cancellationToken = default);
     Task<ErrorOr<CallInfo>> GetCallInfoByConversationUuidAsync(string conversationUuid, CancellationToken cancellationToken = default);
     Task<ErrorOr<string>> SendSmsAsync(CallInfo callInfo, string transcript, string summarizedTranscriptText, CancellationToken cancellationToken = default);
     Task<ErrorOr<Stream>> DownloadRecordingAsync(string recordingUrl, CancellationToken cancellationToken = default);
