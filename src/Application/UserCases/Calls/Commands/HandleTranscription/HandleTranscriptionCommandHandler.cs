b1caa7f6f1d4f6c9d1c34c7161cc5acb33a44808
tr00d <guillaume.faas@live.fr>
2025-11-13 13:56:16 +0100

    fix: use Vonage integrated DI extensions, rename keys in appsettings

--- src/Application/UserCases/Calls/Commands/HandleTranscription/HandleTranscriptionCommandHandler.cs
+++ src/Application/UserCases/Calls/Commands/HandleTranscription/HandleTranscriptionCommandHandler.cs
@@ -31,8 +31,7 @@ internal sealed class HandleTranscriptionCommandHandler : IRequestHandler<Handle
             webhookRequest.TranscriptionUrl,
             cancellationToken);
 
-        var transcriptionResult = downloadResult.Value;
-        var transcriptText = transcriptionResult.Channels[0].ExtractTranscript();
+        var transcriptText = downloadResult.Value;
 
         var summarizedTranscriptWitMCPResult = await _mcpService.ProcessTranscriptWithMcpAsync(transcriptText, cancellationToken);
         var summarizedTranscriptText = summarizedTranscriptWitMCPResult.Value;
--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -6,6 +6,7 @@ using Vonage.Request;
 using Vonage_SSW_Workshop.Infrastructure.Vonage;
 using Vonage_SSW_Workshop.Application.Common.Interfaces;
 using Vonage_SSW_Workshop.Infrastructure.MCP;
+using Vonage.Extensions;
 
 namespace Vonage_SSW_Workshop.Infrastructure;
 
@@ -14,35 +15,13 @@ public static class DependencyInjection
     public static void AddInfrastructure(this IHostApplicationBuilder builder)
     {
         var services = builder.Services;
-
-        builder.Services.Configure<VonageSettings>(
-            builder.Configuration.GetSection(VonageSettings.SectionName));
-
-        services.AddSingleton(sp =>
-        {
-            var settings = sp.GetRequiredService<IOptions<VonageSettings>>().Value;
-            var privateKey = GetPrivateKeyContent(settings.ApplicationKey);
-            var credentials = Credentials.FromAppIdAndPrivateKey(
-                settings.ApplicationId,
-                privateKey);
-            return new VonageClient(credentials);
-        });
-
-        services.AddScoped<IVonageAuthenticatedHttpClient, VonageAuthenticatedHttpClient>();
+        builder.Services.AddVonageClientScoped(builder.Configuration);
+        builder.Services.Configure<WorkshopSettings>( builder.Configuration.GetSection(WorkshopSettings.SectionName));
         services.AddScoped<IVonageService, VonageService>();
-
         services.AddHttpClient<IMcpService, McpService>(client =>
         {
             client.BaseAddress = new Uri("http://localhost:5000/mcp");
         });
-
         services.AddSingleton(TimeProvider.System);
-    }
-
-    private static string GetPrivateKeyContent(string applicationKey)
-    {
-        if (File.Exists(applicationKey))
-            return File.ReadAllText(applicationKey);
-        return applicationKey;
     }
 }
