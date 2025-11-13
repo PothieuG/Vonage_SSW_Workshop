7c99d08d942c642dfb475b5c95c552a9387c9693
tr00d <guillaume.faas@live.fr>
2025-11-13 16:38:37 +0100

    refactor: apply command-query separation on SupabaseStorageService

--- src/Application/Common/Interface/ISupabaseStorageService.cs
+++ src/Application/Common/Interface/ISupabaseStorageService.cs
@@ -1,16 +1,17 @@
 using ErrorOr;
+using MediatR;
 
 namespace Vonage_SSW_Workshop.Application.Common.Interfaces;
 
 public interface ISupabaseStorageService
 {
-    Task<ErrorOr<string>> UploadTextAsync(
+    Task<ErrorOr<Unit>> UploadTextAsync(
         string content,
         string fileName,
         string folderPath,
         CancellationToken cancellationToken = default);
 
-    Task<ErrorOr<string>> UploadAudioAsync(
+    Task<ErrorOr<Unit>> UploadAudioAsync(
         Stream audioStream,
         string fileName,
         string folderPath,
